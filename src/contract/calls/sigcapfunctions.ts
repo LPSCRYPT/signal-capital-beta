aa39f96f981005873d89e8bad7619f0089b016f0
Nancy Stone <lpscrypt@gmail.com>
2022-07-05 13:46:28 +0200

    deployed to rinkeby testnet

--- src/contract/calls/sigcapfunctions.ts
+++ src/contract/calls/sigcapfunctions.ts
@@ -1,120 +1,124 @@
-import { useCallback, useMemo, useState } from 'react';
-import { useContract, useContractWrite, useProvider, useSigner } from 'wagmi';
-import sigcapABI from '../abis/SignalCapital.json';
-import { sigcapAddress } from '../../ref/addresses';
-import { BigNumber } from 'ethers';
+import { useCallback, useMemo, useState } from "react";
+import { useContract, useContractWrite, useProvider, useSigner } from "wagmi";
+import sigcapABI from "../abis/SignalCapital.json";
+import { sigcapAddress } from "../../ref/addresses";
+import { BigNumber } from "ethers";
 
 const enum callSignature {
-  addAccount = 'addAccount',
-  addNewSignal = 'addNewSignal',
-  signalExisting = 'signalExisting',
-  withdrawPoints = 'withdrawPoints',
+	addAccount = "addAccount",
+	addNewSignal = "addNewSignal",
+	signalExisting = "signalExisting",
+	withdrawPoints = "withdrawPoints"
 }
 
 export const useAddAccount = (name: string) => {
-  const args = useMemo(() => [name], [name]);
+	const args = useMemo(() => [name], [name]);
 
-  const { data, isError, isLoading, writeAsync } = useContractWrite({
-    addressOrName: sigcapAddress,
-    contractInterface: sigcapABI,
-    functionName: callSignature.addAccount,
-    args: args,
-    onError(error) {
-      console.log('Error', error);
-    },
-    onSuccess(data) {
-      console.log('Success', data);
-    },
-  });
+	const { data, isError, isLoading, writeAsync } = useContractWrite({
+		addressOrName: sigcapAddress,
+		contractInterface: sigcapABI,
+		functionName: callSignature.addAccount,
+		overrides: { gasLimit: 1e7 },
+		args: args,
+		onError(error) {
+			console.log("Error", error);
+		},
+		onSuccess(data) {
+			console.log("Success", data);
+		}
+	});
 
-  const fireAddAccount = useCallback(async () => {
-    try {
-      await writeAsync();
-    } catch (e) {
-      console.log(e);
-    }
-  }, [writeAsync]);
+	const fireAddAccount = useCallback(async () => {
+		try {
+			await writeAsync();
+		} catch (e) {
+			console.log(e);
+		}
+	}, [writeAsync]);
 
-  return fireAddAccount;
+	return fireAddAccount;
 };
 
 export const useAddNewSignal = (name: string, points: number) => {
-  const args = useMemo(() => [name, BigNumber.from(points)], [name, points]);
+	const args = useMemo(() => [name, BigNumber.from(points)], [name, points]);
 
-  const { data, isError, isLoading, writeAsync } = useContractWrite({
-    addressOrName: sigcapAddress,
-    contractInterface: sigcapABI,
-    functionName: callSignature.addNewSignal,
-    args: args,
-    onError(error) {
-      console.log('Error', error);
-    },
-    onSuccess(data) {
-      console.log('Success', data);
-    },
-  });
+	const { data, isError, isLoading, writeAsync } = useContractWrite({
+		addressOrName: sigcapAddress,
+		contractInterface: sigcapABI,
+		functionName: callSignature.addNewSignal,
+		overrides: { gasLimit: 1e7 },
+		args: args,
+		onError(error) {
+			console.log("Error", error);
+		},
+		onSuccess(data) {
+			console.log("Success", data);
+		}
+	});
 
-  const fireAddNewSignal = useCallback(async () => {
-    try {
-      await writeAsync();
-    } catch (e) {
-      console.log(e);
-    }
-  }, [writeAsync]);
+	const fireAddNewSignal = useCallback(async () => {
+		try {
+			await writeAsync();
+		} catch (e) {
+			console.log(e);
+		}
+	}, [writeAsync]);
 
-  return fireAddNewSignal;
+	return fireAddNewSignal;
 };
 
 export const useSignalExisting = (name: string, points: number) => {
-  const args = useMemo(() => [name, BigNumber.from(points)], [name, points]);
+	const args = useMemo(() => [name, BigNumber.from(points)], [name, points]);
 
-  const { data, isError, isLoading, writeAsync } = useContractWrite({
-    addressOrName: sigcapAddress,
-    contractInterface: sigcapABI,
-    functionName: callSignature.signalExisting,
-    args: args,
-    onError(error) {
-      console.log('Error', error);
-    },
-    onSuccess(data) {
-      console.log('Success', data);
-    },
-  });
+	const { data, isError, isLoading, writeAsync } = useContractWrite({
+		addressOrName: sigcapAddress,
+		contractInterface: sigcapABI,
+		functionName: callSignature.signalExisting,
+		overrides: { gasLimit: 1e7 },
+		args: args,
+		onError(error) {
+			console.log("Error", error);
+		},
+		onSuccess(data) {
+			console.log("Success", data);
+		}
+	});
 
-  const fireAddSignalExisting = useCallback(async () => {
-    try {
-      await writeAsync();
-    } catch (e) {
-      console.log(e);
-    }
-  }, [writeAsync]);
+	const fireAddSignalExisting = useCallback(async () => {
+		try {
+			await writeAsync();
+		} catch (e) {
+			console.log(e);
+		}
+	}, [writeAsync]);
 
-  return fireAddSignalExisting;
+	return fireAddSignalExisting;
 };
 
 export const useWithdrawPoints = (name: string, points: number) => {
-  const args = useMemo(() => [name, BigNumber.from(points)], [name, points]);
+	const args = useMemo(() => [name, BigNumber.from(points)], [name, points]);
 
-  const { data, isError, isLoading, writeAsync } = useContractWrite({
-    addressOrName: sigcapAddress,
-    contractInterface: sigcapABI,
-    functionName: callSignature.withdrawPoints,
-    args: args,
-    onError(error) {
-      console.log('Error', error);
-    },
-    onSuccess(data) {
-      console.log('Success', data);
-    },
-  });
+	const { data, isError, isLoading, writeAsync } = useContractWrite({
+		addressOrName: sigcapAddress,
+		contractInterface: sigcapABI,
+		functionName: callSignature.withdrawPoints,
+		overrides: { gasLimit: 1e7 },
+		args: args,
+		onError(error) {
+			console.log("Error", error);
+		},
+		onSuccess(data) {
+			console.log("Success", data);
+		}
+	});
 
-  const fireWithdrawPoints = useCallback(async () => {
-    try {
-      await writeAsync();
-    } catch (e) {
-      console.log(e);
-    }
-  }, [writeAsync]);
+	const fireWithdrawPoints = useCallback(async () => {
+		try {
+			await writeAsync();
+		} catch (e) {
+			console.log(e);
+		}
+	}, [writeAsync]);
 
-  return fireWithdrawPoints;
+	return fireWithdrawPoints;
 };
